15c2cbd695cb9a448d4b4b1e2ffa07f1c848dda5
zachatk <github@cyberneticvoid.com>
2023-10-25 13:58:43 -0700

    initial

---
+++ class04/challenge04.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script Name:                  Script Name
+# Author:                       Your Name
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main

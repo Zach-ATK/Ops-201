c0daf3258a59cbac07e3b9d670c5968ae9144fc3
zachatk <github@cyberneticvoid.com>
2023-10-25 13:58:09 -0700

    initial commit

---
+++ class03/functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script Name:                  loginhist
+# Author:                       Zachariah Woodbridge
+# Date of latest revision:      25/10/2023
+# Purpose:                      Challenge 03
+
+# Declaration of variables
+YLW='\033[1;33m' # establishes yellow color
+NC='\033[0m' # resets the color to default
+# Declaration of functions
+loginhist () {
+ last
+ echo -e "${YLW}This is the login history${NC}"   
+}
+# Main
+echo -e "${YLW}One......${NC}"
+loginhist
+echo -e "${YLW}Two......${NC}"
+loginhist
+echo -e "${YLW}Three......${NC}"
+loginhist
+# End

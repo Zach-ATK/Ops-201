30a4aad6066b3a7e09dcf0937d1f8c3c47494d29
Zachariah Woodbridge <94998630+Z-Zachattack@users.noreply.github.com>
2023-10-30 17:36:31 -0700

    Update makerbot3k.sh

--- class06/makerbot3k.sh
+++ class06/makerbot3k.sh
@@ -4,17 +4,18 @@
 # Author:                       Zachariah Woodbridge
 # Date of latest revision:      30/10/2023
 # Purpose:                      Ops Challenge: Class 06 - Making files or Folders
-#
 
 # Declaration of variables
 array=()
 # Declaration of functions
 
 # Main
+# A loop to bring the user back to the top until they exit
 while true; do
     echo " "
     echo "Are you looking to find & create a file or directory?"
     read -p "(type 1 for file or 2 for dir):" num
+    # If then conditional, this creates the file if the person chooses it
     if [ "$num" == 1 ]; then
     read -p "A file huh? What file?" file
         if [ -e "$file" ]; then
@@ -26,6 +27,7 @@ while true; do
             array+=("$file")
             sleep 1
         fi
+    # If then conditional, this creates the directory if the person chooses it
     elif [ "$num" == 2 ]; then
     read -p "A directory huh? What directory?" dir
         if [ -e "$dir" ]; then
@@ -35,9 +37,11 @@ while true; do
             array+=("$dir")
             echo "Directory '$dir' created."
         fi
+    # simple command for when the user selects the wrong option
     else
     echo "now you are really wasting my time."
     fi
+    # array, this could have been done with ls, but I needed to insert an array
     echo "these are your current changes:"
     for chg in "${array[@]}"; do
         echo "$chg"

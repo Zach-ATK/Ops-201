9e4c46ad069a5d74ecb25def9cd6edc5213b17e1
Zachariah Woodbridge <94998630+Z-Zachattack@users.noreply.github.com>
2023-11-02 12:22:22 -0700

    Update Logs.bat

--- class09/Logs.bat
+++ class09/Logs.bat
@@ -1,11 +1,18 @@
+Rem Script Name:                  Logging Logs
+Rem Author:                       Zachariah Woodbridge
+Rem Date of latest revision:      02/11/2023
+Rem Purpose:                      Ops Challenge - Log Retrieval via Powershell
+Rem                               filter logs, create .txt files
+
+
 Rem 1: Output all events from the System event log that occurred in the last 24 hours to a file on your desktop named last_24.txt:
-Get-WinEvent -LogName System -MaxEvents 200 -FilterHashtable @{LogName='System'; StartTime=(Get-Date).AddHours(-24)} | Format-List | Out-File "$env:USERPROFILE\Desktop\last_24.txt"
+Get-WinEvent -LogName System -FilterHashtable @{LogName='System'; StartTime=(Get-Date).AddHours(-24)} | Format-List | Out-File "$env:USERPROFILE\Desktop\last_24.txt"
 
 Rem 2: Output all "error" type events from the System event log to a file on your desktop named errors.txt:
-Get-WinEvent -LogName System -MaxEvents 200 | Where-Object { $_.LevelDisplayName -eq 'Error' } | Format-List | Out-File "$env:USERPROFILE\Desktop\errors.txt"
+Get-WinEvent -LogName System | Where-Object { $_.LevelDisplayName -eq 'Error' } | Format-List | Out-File "$env:USERPROFILE\Desktop\errors.txt"
 
 Rem 3: Print to the screen all events with ID of 16 from the System event log:
-Get-WinEvent -LogName System -MaxEvents 200 | Where-Object { $_.Id -eq 16 } | Format-Table -AutoSize
+Get-WinEvent -LogName System | Where-Object { $_.Id -eq 16 } | Format-Table -AutoSize
 
 Rem 4: Print to the screen the most recent 20 entries from the System event log:
 Get-WinEvent -LogName System -MaxEvents 20 | Format-Table -AutoSize

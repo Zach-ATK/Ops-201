8b49794defe2cf199a9d3967326ccc20602973d1
Zachariah Woodbridge <94998630+Z-Zachattack@users.noreply.github.com>
2023-11-03 13:55:31 -0700

    Update and rename save.ps1 to zachsfile.ps1

--- class14/save.ps1
+++
@@ -1,4 +0,0 @@
-She emphasized the importance of being process-oriented and ensuring that the basics are done correctly to support business scalability and optimization.
-Raya also mentioned her role as a project manager and her work in cybersecurity for a nonprofit called Partnership to End Addiction in New York.
-Her responsibilities included helping non-technical users understand web browsers and resolving issues related to a new platform.
-Raya provided insights into her interview experience and discussed the need for continuous education and networking in the field of cybersecurity.
